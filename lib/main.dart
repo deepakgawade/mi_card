224678d7cb50d68a1c6c7f06b085732dc9be3b37
deepakgawade <deepakgawade22@gmail.com>
2021-05-01 21:12:05 +0530

    Added New Resouces

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/image.JPG'),
+              ),
+              Text(
+                'Deepak Gawade',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro',
+                    letterSpacing: 2.5,
+                    color: Colors.teal.shade100),
+              ),
+              SizedBox(
+                width: 150.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade700,
+                    ),
+                    horizontalTitleGap: 30.0,
+                    title: Text(
+                      '+91 7798182293',
+                      style: TextStyle(
+                          fontSize: 21.0,
+                          fontFamily: 'Source Sans Pro',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal.shade900),
+                    ),
+                  )),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal.shade700,
+                    ),
+                    horizontalTitleGap: 30.0,
+                    title: Text(
+                      'deepakgawdae22@gmail.com',
+                      style: TextStyle(
+                          fontSize: 21.0,
+                          fontFamily: 'Source Sans Pro',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal.shade900),
+                    ),
+                  ))
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
